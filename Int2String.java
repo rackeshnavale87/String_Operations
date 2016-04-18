6e36b3d1cd613e725af1190018cc420898cfb51d
Rakesh_Navale <rackeshnavale87@users.noreply.github.com>
2016-04-17 19:48:22 -0700

    Create Int2String.java

---
+++ Int2String.java
@@ -0,0 +1,39 @@
+public class Int2String 
+{
+	public static void main(String[] args) 
+	{
+		int inum = -123;
+		String snum = "";
+		if(inum < 0)
+		{
+			snum = "-";
+			snum = snum + getNum((-1)*inum);
+		}
+		else
+		{
+			snum = getNum(inum);
+		}
+		System.out.println("String representation :"+ snum);
+	}
+
+	private static String getNum(int temp) 
+	{
+		int value = temp, ans = 0, i = 0;
+		while(value > 0)
+		{
+			value = value / 10;
+			i++;
+		}
+		char chnum[] = new char[i];
+		value = i  = 0;
+		while(temp > 0)
+		{
+			value = temp % 10;
+			temp = temp/10;
+			chnum[chnum.length -1 - i] = (char) ('0' + value);
+			i++;
+		}
+		return new String(chnum);
+	}
+
+}
